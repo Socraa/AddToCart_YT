227d45f5b75fad46db760cd59d25e40cc670c1fe
Socraa <Jaidyelle12@gmail.com>
2026-02-07 20:34:26 +0800

    Skeleton for CSS and HTML

---
+++ main.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("input_field");
+const add_btn = document.getElementById("add_btn");
+
+add_btn.addEventListener('click', () => {
+    let inputValue = input.value;
+    console.log(inputValue);
+})
